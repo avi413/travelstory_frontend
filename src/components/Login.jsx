d6abe80103fcff7ffe9f4c78b353bcda8ea19ce3
Avi Dalal <avi413@gmail.com>
2022-09-21 15:06:14 +0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import jwt_decode from 'jwt-decode';
+import { client } from '../container/client';
 
 function Login() {
+  const navigate = useNavigate();
+  const responsGoogle = (googleUser) => {
+    var decodedUser = jwt_decode(googleUser.credential);
+    localStorage.setItem('tavelstory-user', JSON.stringify(decodedUser));
+    const { name, sub, picture } = decodedUser;
+
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    };
+
+    client.createIfNotExists(doc)
+    .then(() => {
+      navigate('/', { replace: true })
+    })
+  };
   return (
-    <div>Login</div>
-  )
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-ful h-full'>
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+        <div className='absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} alt='logo' width='130px' />
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              render={(renderProps) => (
+                <button
+                  type='button'
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className='mr-4' /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responsGoogle}
+              onError={responsGoogle}
+              cookiePolicy='single_host_origin'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import reactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
-reactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+    <Router>
+      <App />
+    </Router>{' '}
+  </GoogleOAuthProvider>
 );
